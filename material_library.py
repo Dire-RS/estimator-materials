c88c0864c7a8aa889e9d6e0fd7827fcb28f163a0
Dire-RS <riosakya2023@gmail.com>
2025-06-17 16:10:50 -0700

    Create material_library.py

---
+++ material_library.py
@@ -0,0 +1,14 @@
+MATERIALS = {
+    "12_awg_thhn_500ft": {
+        "vendors": {
+            "Rexel": 87.99,
+            "Home Depot": 119.00
+        }
+    },
+    "14_awg_thhn_500ft": {
+        "vendors": {
+            "Rexel": 59.54,
+            "Home Depot": 78.00
+        }
+    }
+}
