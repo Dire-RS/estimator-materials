994a0d61f951823b3f88a03d84d3880e3cdd9b07
Dire-RS <riosakya2023@gmail.com>
2025-06-18 18:54:32 -0700

    Update material_library.py

--- material_library.py
+++ material_library.py
@@ -1,11 +1,13 @@
 MATERIALS = {
     "12_awg_thhn_500ft": {
+        "name": "#12 THHN (500ft roll)",
         "vendors": {
             "Rexel": 87.99,
             "Home Depot": 119.00
         }
     },
     "14_awg_thhn_500ft": {
+        "name": "#14 THHN (500ft roll)",
         "vendors": {
             "Rexel": 59.54,
             "Home Depot": 78.00
